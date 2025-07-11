eedd209c0b8749de17b6f5b69f95b5e3982259f0
the-flying-cow <anant07singh2004@gmail.com>
2025-07-11 20:19:02 +0530

    Modified code by removing redundant lines and cleaned widget configurations.

--- currency_conv.py
+++ currency_conv.py
@@ -1,50 +1,43 @@
 import tkinter as tk
-from tkinter import *
 from tkinter import ttk
 
-win = Tk()
+win = tk.Tk()
 win.title('Currency-Converter')
 
-center_frame = Frame(win,bg='grey',width=400,height=400,relief=RIDGE,borderwidth=5)
+center_frame = tk.Frame(win,bg='grey',width=400,height=400,relief=tk.RIDGE,borderwidth=5)
 center_frame.pack(expand=True)
 center_frame.pack_propagate(False)
 
-label = Label(center_frame,text='Currency-Converter',bg='sky blue',font=('TkDefaultFont', 12),justify=CENTER,relief=RAISED)
+label = tk.Label(center_frame,text='Currency-Converter',bg='sky blue',font=('TkDefaultFont', 12),justify=tk.CENTER,relief=tk.RAISED)
 label.pack(pady=10)
-label.pack_propagate(False)
 
-from_frame = Frame(center_frame,border=10,bg='light grey',height=45,width=350)
+def handle_keypress():
+    print(1)
+
+from_frame = tk.Frame(center_frame,border=10,bg='light grey',height=45,width=350)
 from_frame.pack(expand=False,pady=10)
 from_frame.pack_propagate(False)
-from_label = Label(from_frame,text='FROM :',bg='sky blue',border=5,relief=FLAT,font=('Arial', 10))
-from_label.pack(expand=True)
-from_label.pack_propagate(False)
+from_label = tk.Label(from_frame,text='FROM :',bg='sky blue',border=5,relief=tk.FLAT,font=('Arial', 10))
 from_label.place(x=-5)
-
 from_box = ttk.Combobox(from_frame,values=['INR','DOLLAR','POUND'])
-from_box.pack(expand=True)
-from_box.pack_propagate(False)
 from_box.place(x=190)
 
-to_frame = Frame(center_frame,border=10,bg='light grey',height=45,width=350)
+to_frame = tk.Frame(center_frame,border=10,bg='light grey',height=45,width=350)
 to_frame.pack(expand=False,pady=20)
 to_frame.pack_propagate(False)
-to_label = Label(to_frame,text='TO :',bg='sky blue',border=5,relief=FLAT,font=('Arial', 10))
-to_label.pack(expand=True)
-to_label.pack_propagate(False)
+to_label = tk.Label(to_frame,text='TO :',bg='sky blue',border=5,relief=tk.FLAT,font=('Arial', 10))
 to_label.place(x=-5)
-
 to_box = ttk.Combobox(to_frame,values=['INR','DOLLAR','POUND'])
-to_box.pack(expand=True)
-to_box.pack_propagate(False)
 to_box.place(x=190)
 
-text_box = Entry(center_frame,bg='yellow',border=5,relief=RIDGE,font=('TkDefaultFont', 12))
+from_box.set('INR')
+to_box.set('DOLLAR')
+
+text_box = tk.Entry(center_frame,bg='yellow',border=5,relief=tk.RIDGE,font=('TkDefaultFont', 12))
 text_box.pack(expand=True)
-text_box.pack_propagate(False)
 
-convert_button = Button(center_frame,text='CONVERT',bg='red',width=10,height=1)
+convert_button = tk.Button(center_frame,text='CONVERT',bg='red',width=10,height=1)
 convert_button.pack(expand=True)
-convert_button.pack_propagate(False)
 
+win.bind("<Key>",handle_keypress)
 win.mainloop()
