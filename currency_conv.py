e483b2381cd64fa4ff62651ffd0471e273bb768a
the-flying-cow <anant07singh2004@gmail.com>
2025-07-17 17:34:26 +0530

    feat: API integration for currency conversion Modify the exchange function to work using live rates using webAPIs

--- currency_conv.py
+++ currency_conv.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 
+import requests 
+
 win = tk.Tk()
 win.title('Currency-Converter')
 
@@ -22,24 +24,27 @@ def exchange():
     try:
         from_amount = float(value)
 
-        rates_dict = {'INR':{'USD': 0.012, 'GBP': 0.0095, 'INR': 1},
-                  'USD':{'INR': 83.33, 'GBP': 0.79, 'USD': 1},
-                  'GBP':{'INR': 105.26, 'USD': 1.26, 'GBP': 1}}
-        
-        rate = rates_dict[from_value][to_value]
-        to_amount = rate*from_amount
+        api_key = "YOUR-API-KEY"
+        url = f"https://v6.exchangerate-api.com/v6/{api_key}/pair/{from_value}/{to_value}/{from_amount}"
 
-        result_label.config(text=f"{to_amount:.3f}")
+        response = requests.get(url)
+        data = response.json()
+
+        if data['result']=='success':
+            to_amount = data["conversion_result"]
+            result_label.config(text=f"{to_amount:.4f}")
+        else:
+            result_label.config(text="API Error!")
 
     except:
-        result_label.config(text='Please input valid numerical values')
+        result_label.config(text='Please input valid numerical values!')
 
 from_frame = tk.Frame(center_frame,border=10,bg='light grey',height=45,width=350)
 from_frame.pack(expand=False,pady=10)
 from_frame.pack_propagate(False)
 from_label = tk.Label(from_frame,text='FROM :',bg='sky blue',border=5,relief=tk.FLAT,font=('Arial', 10))
 from_label.place(x=-5)
-from_box = ttk.Combobox(from_frame,values=['INR','USD','GBP'])
+from_box = ttk.Combobox(from_frame,values=['USD', 'INR', 'EUR', 'GBP', 'JPY', 'CAD', 'AUD', 'CHF', 'CNY', 'NZD'])
 from_box.place(x=190)
 
 to_frame = tk.Frame(center_frame,border=10,bg='light grey',height=45,width=350)
@@ -47,7 +52,7 @@ to_frame.pack(expand=False,pady=20)
 to_frame.pack_propagate(False)
 to_label = tk.Label(to_frame,text='TO :',bg='sky blue',border=5,relief=tk.FLAT,font=('Arial', 10))
 to_label.place(x=-5)
-to_box = ttk.Combobox(to_frame,values=['INR','USD','GBP'])
+to_box = ttk.Combobox(to_frame,values=['USD', 'INR', 'EUR', 'GBP', 'JPY', 'CAD', 'AUD', 'CHF', 'CNY', 'NZD'])
 to_box.place(x=190)
 
 #default selection
